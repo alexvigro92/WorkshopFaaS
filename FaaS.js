a40c7a6a2b35bbc95708281fcec069e7757d6009
Alejandro Villa <alexvigro92@gmail.com>
2019-03-22 08:47:33 -0600

    added FaaS code

---
+++ FaaS.js
@@ -0,0 +1,31 @@
+const watson = require('watson-developer-cloud'); 
+
+const workspace_id = "your workspace id";
+
+const assistant = new watson.AssistantV1({
+    username: "your username",
+    password: "your password",
+    url: "your url",
+    version: "2018-09-20"
+});
+
+function main(params) {
+    console.log(params.context)
+    console.log(params.question)
+	return new Promise((resolve, reject) => {
+        let message = {
+            "message":{
+                "text": params.question
+            },
+            "context":{...params.context}
+        };
+        assistant.message({
+            workspace_id: workspace_id,
+            input: message.message,
+            context: message.context
+        }, (err, response) => {
+                let question = response.context.question;
+                resolve(response)
+        }); 
+    });
+}
